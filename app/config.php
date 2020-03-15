f6a2a55baf49e8c02f7d7dad4eba0d4f4b08d5fe
Antoine d'Otreppe <antoine@stellr.be>
2020-03-15 23:15:54 +0100

    Always reuse the same salt

--- app/config.php
+++ app/config.php
@@ -7,11 +7,20 @@ define('STORAGE_DIR', '../storage');
 # The date pattern used to generate the filename
 define('LOG_FILENAME_PATTERN', 'Y-m');
 
+# Used to salt the sha512 for the user directory. Use any random value
+define('USER_DIR_SALT', 'hello this is some salt');
+
 # What type of cipher is used. To date, only "aes-256-gcm" is supported
 define('CIPHER', 'aes-256-gcm');
 
 # How many iterations to use while deriving the password to an AES key
-define('PBKDF2_ITERATIONS', 30);
+define('PBKDF2_ITERATIONS', 10000);
 
 # How many random bytes to use as salt for pbkdf2 password derivation
-define('PBKDF2_SALT_BYTES', 16);
+define('PBKDF2_SALT_BYTES', 32);
+
+# How many random bytes to use for authentication payload
+define('AUTH_PAYLOAD_BYTES', 32);
+
+# Name of the file storing the pbkdf2 salt in the user directory. Anything not ending in .log is fine.
+define('USER_SETTINGS_FILE', 'settings.json');
--- app/log.php
+++ app/log.php
@@ -24,6 +24,88 @@ if (!(defined('PBKDF2_ITERATIONS') && is_numeric(PBKDF2_ITERATIONS))) {
     die("Missing or invalid 'PBKDF2_ITERATIONS constant in config.php. A good starting point would be 20");
 }
 
+if (!defined('USER_DIR_SALT')) {
+    die("Missing 'USER_DIR_SALT' constant in config.php.");
+}
+
+if (!defined('USER_SETTINGS_FILE')) {
+    die("Missing 'USER_SETTINGS_FILE' constant in config.php.");
+}
+
+if (!(defined('AUTH_PAYLOAD_BYTES') && is_numeric(AUTH_PAYLOAD_BYTES))) {
+    # TODO What's a good starting point for iteration count, actually?
+    die("Missing or invalid 'AUTH_PAYLOAD_BYTES constant in config.php. A good starting point would be 20");
+}
+
+class Settings {
+    public $auth_iv;
+    public $auth_tag;
+    public $auth_payload;
+    public $pbkdf2_salt;
+    public $pbkdf2_iterations;
+
+    private $key = null;
+
+    const CIPHER = 'aes-256-gcm';
+
+    public function initialize_new_user($password) {
+        $this->pbkdf2_salt = openssl_random_pseudo_bytes(PBKDF2_SALT_BYTES);
+        $this->pbkdf2_iterations = PBKDF2_ITERATIONS;
+
+        $this->generate_key($password);
+        $this->auth_iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(CIPHER));
+
+        $cleartext_auth_payload = openssl_random_pseudo_bytes(AUTH_PAYLOAD_BYTES);
+        $this->auth_payload = $this->encrypt($cleartext_auth_payload, $this->auth_iv, $this->auth_tag);
+    }
+
+    public function authenticate($password) {
+        $this->generate_key($password);
+        return $this->decrypt($this->auth_payload, $this->auth_iv, $this->auth_tag) !== false;
+    }
+
+    public function generate_key($password) {
+        if ($this->key !== null) {
+            $this->key = hash_pbkdf2('sha512', $password, $this->pbkdf2_salt, $this->pbkdf2_iterations);
+        }
+    }
+
+    public function encrypt($payload, $iv, &$tag) {
+        assert($this->key !== null);
+        return openssl_encrypt($payload, self::CIPHER, $this->key, 0, $iv, $tag);
+    }
+
+    public function decrypt($payload, $iv, $tag) {
+        assert($this->key !== null);
+        return openssl_decrypt($payload, self::CIPHER, $this->key, 0, $iv, $tag);
+    }
+
+    public function to_json() {
+        $serialized_settings = [];
+
+        $serialized_settings['auth_iv'] = base64_encode($this->auth_iv);
+        $serialized_settings['auth_tag'] = base64_encode($this->auth_tag);
+        $serialized_settings['auth_payload'] = base64_encode($this->auth_payload);
+        $serialized_settings['pbkdf2_salt'] = base64_encode($this->pbkdf2_salt);
+        $serialized_settings['pbkdf2_iterations'] = $this->pbkdf2_iterations;
+
+        return json_encode($serialized_settings);
+    }
+
+    public static function from_json($json_string) {
+        $serialized_settings = json_decode($json_string, true);
+
+        $settings = new Settings();
+        $settings->auth_iv = base64_decode($serialized_settings['auth_iv']);
+        $settings->auth_tag = base64_decode($serialized_settings['auth_tag']);
+        $settings->auth_payload = base64_decode($serialized_settings['auth_payload']);
+        $settings->pbkdf2_salt = base64_decode($serialized_settings['pbkdf2_salt']);
+        $settings->pbkdf2_iterations = $serialized_settings['pbkdf2_iterations'];
+
+        return $settings;
+    }
+}
+
 # Require basic authentication
 if (!isset($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
     header('WWW-Authenticate: Basic realm="Captain\'s Log"');
@@ -32,24 +114,44 @@ if (!isset($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
 }
 
 # Derive the user/password to find out the user's home directory
-$user_hash = hash('sha512', $_SERVER['PHP_AUTH_USER'].':'.$_SERVER['PHP_AUTH_PW']);
+$user_hash = hash('sha512', $_SERVER['PHP_AUTH_USER'].':'.USER_DIR_SALT);
 
 # This is safe, because the hash can only contain alphanumeric characters
-define('USER_DIR', STORAGE_DIR.'/'.$user_hash);
+$user_dir = STORAGE_DIR.'/'.$user_hash;
+$user_settings_file = $user_dir.'/'.USER_SETTINGS_FILE;
 
 # The permission system is very simple.
 # If the folder does exist, the user is allowed.
 # Otherwise, the user is allowed if and only if we can create a new folder.
 # So for example, if you want to restrict access to only one user,
 # create his directory first, and then lock the STORAGE_DIR permissions.
-if (!is_dir(USER_DIR)) {
+$start = hrtime(true);
+if (!is_dir($user_dir)) {
+    # User does not exist, and...
     if (is_writeable(STORAGE_DIR)) {
-        mkdir(USER_DIR);
+        # ... we can create it
+        mkdir($user_dir);
+
+        # We're going to generate pbkdf salt, and a randon payload to encrypt.
+        # This encrypted payload will allow us to authenticate the user later
+        $settings = new Settings();
+        $settings->initialize_new_user($_SERVER['PHP_AUTH_PW']);
+        file_put_contents($user_settings_file, $settings->to_json());
     }
     else {
+        # ... we can't create the user
         die("This user does not exist and cannot be created.");
     }
 }
+else {
+    $settings = Settings::from_json(file_get_contents($user_settings_file));
+    
+    if (!$settings->authenticate($_SERVER['PHP_AUTH_PW'])) {
+        die("Invalid password");
+    }
+}
+$end = hrtime(true);
+$time_to_key = ($end - $start) / 1000000.0;
 
 # GET lists the recent log entries
 # POST creates a new log entry
@@ -66,31 +168,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     # Also, we're clustering logs by month
     $basename = gmdate(LOG_FILENAME_PATTERN, $current_time);
-    $filepath = USER_DIR.'/'.$basename.'.log';
+    $filepath = $user_dir.'/'.$basename.'.log';
 
     # And finally, the full utc iso date format for the log entry itself
     $date = gmdate('c', $current_time);
     
-    # Generate the key based on the user's password, using pbkdf2 derivation
-    $salt = openssl_random_pseudo_bytes(PBKDF2_SALT_BYTES);
-    $key = hash_pbkdf2('sha256', $_SERVER['PHP_AUTH_PW'], $salt, PBKDF2_ITERATIONS);
-
     # Random IV
     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(CIPHER));
 
     # "0" is options. I chose to keep the padding.
-    $encrypted_payload = openssl_encrypt($cleartext_payload, CIPHER, $key, 0, $iv, $tag);
+    $encrypted_payload = $settings->encrypt($cleartext_payload, $iv, $tag);
     
     # base64 everything so we can write it in a text file
-    $b64_salt = base64_encode($salt);
     $b64_iv = base64_encode($iv);
     $b64_tag = base64_encode($tag);
     $b64_payload = base64_encode($encrypted_payload);
 
     $csv_array = [
         $date,
-        $b64_salt,
-        PBKDF2_ITERATIONS,
         $b64_iv,
         $b64_tag,
         $b64_payload
@@ -120,7 +215,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $since = $_GET['since'] || (time() - 86400 * 30);
 
 # List all files for this user
-$log_files = scandir(USER_DIR);
+$log_files = glob($user_dir.'/*.log');
 
 $decrypted_entries = [];
 $errors = [];
@@ -128,30 +223,24 @@ $errors = [];
 # TODO Improve the "since" selection.
 # For now, we'll simply parse all files and filter afterwards.
 # Something smarter could be done with the file's mtime, I guess
-foreach ($log_files as $basename) {
-    $filepath = USER_DIR.'/'.$basename;
-
+$start = hrtime(true);
+foreach ($log_files as $filepath) {
     $file_handle = fopen($filepath, 'r');
 
     if ($file_handle !== false) {
         while (($csv_array = fgetcsv($file_handle)) !== false) {
             [
                 $date,
-                $b64_salt,
-                $pbkdf2_iterations,
                 $b64_iv,
                 $b64_tag,
                 $b64_payload
             ] = $csv_array;
 
-            $salt = base64_decode($b64_salt);
             $iv = base64_decode($b64_iv);
             $tag = base64_decode($b64_tag);
             $payload = base64_decode($b64_payload);
             
-            $key = hash_pbkdf2('sha256', $_SERVER['PHP_AUTH_PW'], $salt, $pbkdf2_iterations);
-
-            $decrypted_payload = openssl_decrypt($payload, CIPHER, $key, 0, $iv, $tag);
+            $decrypted_payload = $settings->decrypt($payload, $iv, $tag);
             $decrypted_entries[] = [$date, $decrypted_payload];
         }
     }
@@ -161,6 +250,8 @@ foreach ($log_files as $basename) {
 
     fclose($file_handle);
 }
+$end = hrtime(true);
+$time_to_decrypt = ($end - $start) / 1000000.0;
 
 ?>
 <!DOCTYPE html>
@@ -193,5 +284,9 @@ foreach ($log_files as $basename) {
             <?php endforeach ?>
             </tbody>
         </table>
+        <footer>
+            <span>Time to generate key: <?= $time_to_key ?></span>
+            <span>Time to decrypt log: <?= $time_to_decrypt ?></span>
+        </footer>
     </body>
 </html>
